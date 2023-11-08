0993f05a3fe879b6a51f4d7bc857b66be63ee877
mateusz.swiderski <mateusz.swiderski@itm.com.pl>
2023-11-08 13:47:16 +0100

    init2

--- api/main.py
+++ api/main.py
@@ -20,7 +20,7 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 # model.add(tf.keras.layers.Dense(128, activation='relu'))
 # model.add(tf.keras.layers.Dense(10, activation='softmax'))
 # model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-#
+
 # model.fit(x_train, y_train, epochs=9)
 #
 # model.save('handwritten.model2')
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="flex-container">
-    <vue-drawing-canvas  :outputHeight="28" :outputWidth="28" style="border:solid 2px black" :line-width="2" :width="50" :height="50" ref="VueCanvasDrawing" />
-    <button @click="recognizeLetter">Rozpoznaj Cyrfę</button>
-    <div>
-      {{prediction}}
-    </div>
+    <main>
+      <section class="header">
+        <div style="display:flex; align-items: center">
+              <input type="checkbox" v-model="options.quickDraw" />
+               <span>Wyczyść i rozpoznaj po narysowaniu</span>
+        </div>
+        <div style="text-align:right">
+           <button @click="recognizeLetter">Rozpoznaj Cyrfę</button>
+        </div>
+
+      </section>
+      <section class="drawing-ctx">
+        <vue-drawing-canvas  :outputHeight="28" :outputWidth="28" style=" border:solid 2px #00000040;" :line-width="12" :width="200" :height="200" ref="VueCanvasDrawing" />
+        <div class="prediction-result">
+          <section class="prediction">
+            Prediction: {{prediction}}
+          </section>
+        </div>
+      </section>
+
+    </main>
   </div>
 </template>
 
@@ -39,7 +55,10 @@ export default {
     return {
       model: null,
       service:new this.ServiceClass(),
-      prediction:null
+      prediction:null,
+      options:{
+        quickDraw:true
+      }
 
     };
   },
@@ -52,36 +71,96 @@ export default {
       const blob = await fetch(imageData).then(res => res.blob());
 
       formData.append('file', blob, 'image.png');
-
+      this.prediction = '⏳'
       this.service.getPrediction(formData).then(res => {
         this.prediction = res.data?.prediction
         this.$refs.VueCanvasDrawing.reset()
+      }).catch(e=> {
+        this.prediction = 'E!'; console.log(e)
       });
     },
   },
   async created() {
+    var drawing = false;
+    this.$nextTick(()=>{
+      const canvas = document.querySelector('#VueDrawingCanvas');
+      canvas.addEventListener('mousedown',()=>{
+        if(!this.options.quickDraw) return;
+
+        drawing = true;
+      })
+      canvas.addEventListener('mouseup',()=>{
+        if(!this.options.quickDraw) return;
+        if(drawing){
+          this.recognizeLetter()
+          drawing = false;
+        }
+      })
+    })
+
   },
 };
 </script>
 <style>
+@font-face {
+  font-family: "Robot";
+  src: url("@/assets/Roboto-Medium.ttf");
+}
+body{
+  background: #70788d;
+  font-family: "Roboto", sans-serif!important;
+  overflow:hidden
+}
 .flex-container{
   width:100vw;
   height:100vh;
   display:flex;
   justify-content: center;
   align-items:center;
-  button{
-  height: 54px;
-    border-radius:0;
-    border:solid 2px black;
+  box-sizing: border-box;
+  main{
+    width:60vw;
+    height:70vh;
+    min-height: 300px;
+    box-sizing: border-box;
+    min-width:800px;
+    max-height:300px;
+    background:white;
+    padding: 8px;
+    color: #70788d;
+    box-shadow:3px 3px 3px #00000030;
+    .header{
+      width:100%;
+      border-bottom: solid 2px #fc716a;
+      height:30px;
+      background:#00000020;
+      font-size:0.8em;
+      display:grid;
+      grid-template-columns: 1fr 1fr;
+      box-sizing: border-box;
+
+    }
+    .drawing-ctx{
+      padding-block:12px;
+    }
   }
-  div{
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    width:50px;
-    height:50px;
+  button{
+    border:solid 2px #00000030;
+    border-radius:0;
+    border-left: solid 3px #00000030;
+    border-right: solid 3px #00000030;
+    width:200px;
+    height:30px;
+    background:#fc716a;
+    color:#fff;
+  }
+  .prediction-result{
+
+
   }
 }
-
+.drawing-ctx{
+  display:flex;
+  gap: 20px;
+}
 </style>
